bb7c893f75a838f691bc61202d60011e96256636
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2019-10-17 16:23:40 +0100

    fix: Update config and readme

--- README.md
+++ README.md
@@ -1,26 +1,31 @@
 # NFS Ganesha
-A user mode nfs server implemented in a container. Supports serving NFS (v3, 4.0, 4.1, 4.1 pNFS, 4.2) and 9P. This container should also be configurable with all of the nfs-ganesha supported FSAL backends.
 
-Currently generates a config for just serving a local path over nfs. However supplying `GANESHA_CONFIGFILE` would allow ganesha to be pointed to a bind mounted config file for other FASLs/more advanced configuration.
+A user mode nfs server implemented in a container.
+Supports serving NFS (v3, v4).
 
-### Versions
-* ganesha: 2.4
-* glusterfs-common: 3.11
+Currently generates a config for just serving a local path over nfs.
+However supplying `GANESHA_CONFIGFILE` would allow ganesha
+to be pointed to a bind mounted config file for other FASLs/more advanced configuration.
 
-### Environment Variables
+## Versions
+
+* ganesha: 2.8.2
+
+## Environment Variables
+
 * `GANESHA_LOGFILE`: log file location
+* `GANESHA_PID`: log file location
 * `GANESHA_CONFIGFILE`: location of ganesha.conf
 * `GANESHA_OPTIONS`: command line options to pass to ganesha
 * `GANESHA_EPOCH`: ganesha epoch value
 * `GANESHA_EXPORT_ID`: ganesha unique export id
 * `GANESHA_EXPORT`: export location
-* `GANESHA_ACCESS`: export access acl list
-* `GANESHA_ROOT_ACCESS`: export root access acl list
 * `GANESHA_NFS_PROTOCOLS`: nfs protocols to support
 * `GANESHA_TRANSPORTS`: nfs transports to support
 * `STARTUP_SCRIPT`: location of a shell script to execute on start
 
-#### Environment Placement in Config File
+### Environment Placement in Config File
+
 ````
 EXPORT
 {
@@ -52,13 +57,17 @@ EXPORT
 }
 ````
 
-### Usage
+## Usage
+
 ```bash
-docker run -d \
---name nfs \
--v /local/export/path:/export \
-mitcdh/nfs-ganesha \
+docker run \
+	-d \
+	--privileged \
+	--name nfs-ganesha-server \
+	-v $(mktemp -d):/export:rw \
+	rtfpessoa/nfs-ganesha:dev
 ```
 
-### Credits
-* [janeczku/docker-nfs-ganesha](https://github.com/janeczku/docker-nfs-ganesha)
+## Credits
+
+* [mitcdh/docker-nfs-ganesha](https://github.com/mitcdh/docker-nfs-ganesha)
--- start.sh
+++ start.sh
@@ -1,18 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 set -e
 
 # Options for starting Ganesha
 : ${GANESHA_LOGFILE:="/dev/stdout"}
+: ${GANESHA_PID:="/var/run/ganesha.pid"}
 : ${GANESHA_CONFIGFILE:="/etc/ganesha/ganesha.conf"}
 : ${GANESHA_OPTIONS:="-N NIV_EVENT"} # NIV_DEBUG
 : ${GANESHA_EPOCH:=""}
-: ${GANESHA_EXPORT_ID:="77"}
+: ${GANESHA_EXPORT_ID:="69"}
 : ${GANESHA_EXPORT:="/export"}
-# : ${GANESHA_ACCESS:="*"}
-# : ${GANESHA_ROOT_ACCESS:="*"}
 : ${GANESHA_NFS_PROTOCOLS:="3,4"}
 : ${GANESHA_TRANSPORTS:="UDP,TCP"}
 
+# Docs: https://github.com/nfs-ganesha/nfs-ganesha/blob/next/src/doc/man/ganesha-export-config.rst
 function bootstrap_config {
 	echo "Bootstrapping Ganesha NFS config"
 	mkdir -p `dirname ${GANESHA_CONFIGFILE}`
@@ -26,11 +27,15 @@ EXPORT
 	# Exported path (mandatory)
 	Path = ${GANESHA_EXPORT};
 	# Pseudo Path (required for NFS v4)
-	Pseudo = ${GANESHA_EXPORT};
+	Pseudo = /;
 	# Required for access (default is None)
 	# Could use CLIENT blocks instead
 	Access_Type = RW;
 	# Exporting FSAL
+
+	SecType = "sys";
+	Squash = No_Root_Squash;
+
 	FSAL {
 		Name = VFS;
 	}
@@ -41,12 +46,16 @@ NFS_Core_Param
 	MNT_Port = 20048;
 	fsid_device = true;
 }
+
 NFSV4
 {
 	Grace_Period = 90;
 }
 
 END
+
+	chmod 0600 ${GANESHA_CONFIGFILE}
+
 }
 
 function bootstrap_export {
@@ -55,47 +64,35 @@ function bootstrap_export {
   	fi
 }
 
+function init_dbus {
+	echo "Starting dbus"
+	rm -f /var/run/dbus/system_bus_socket
+	rm -f /var/run/dbus/pid
+	dbus-uuidgen --ensure
+	dbus-daemon --system --fork
+	sleep 2
+}
+
 function init {
 	echo "Starting rpcbind"
 	/usr/sbin/rpcbind -w
 	sleep 2
-	/usr/sbin/rpc.statd
 
+	/usr/sbin/rpc.statd -L
 	sleep 2
 
-	dbus-daemon --system
+	init_dbus
 	sleep 2
-	#rpcbind || return 0
-	#rpc.statd -L || return 0
-	#rpc.idmapd || return 0
-	#sleep 1
-}
 
-# function init_dbus {
-# 	echo "Starting dbus"
-# 	rm -f /var/run/dbus/system_bus_socket
-# 	rm -f /var/run/dbus/pid
-# 	dbus-uuidgen --ensure
-# 	dbus-daemon --system --fork
-# 	sleep 1
-# }
-
-function startup_script {
+	rm -rf /usr/local/var/run/ganesha
 	mkdir -p /usr/local/var/lib/nfs/ganesha
-
-	if [ -f "${STARTUP_SCRIPT}" ]; then
-  		/bin/sh ${STARTUP_SCRIPT}
-	fi
 }
 
 bootstrap_config
 bootstrap_export
-startup_script
 
 init
 
-
 echo "Starting Ganesha NFS"
-# Do we need this?
-# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib
-exec ganesha.nfsd -F -L ${GANESHA_LOGFILE} -f ${GANESHA_CONFIGFILE} ${GANESHA_ADDITIONAL_OPTIONS}
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib
+exec ganesha.nfsd -F -L ${GANESHA_LOGFILE} -p ${GANESHA_PID} -f ${GANESHA_CONFIGFILE} ${GANESHA_ADDITIONAL_OPTIONS}
